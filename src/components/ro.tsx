fabb3393104154ed2d1edcf01daaf58258499546
smith-chris <kowalski.k@outlook.com>
2019-09-08 17:13:48 +0200

    Added types to basic ping pong example.

--- src/components/ro.tsx
+++ src/components/ro.tsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import ReactDOM from 'react-dom'
-
 import { connect, Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { createEpicMiddleware } from 'redux-observable'
+import { Epic, createEpicMiddleware } from 'redux-observable'
 import { mapTo, delay } from 'rxjs/operators'
 
 const PING = 'PING'
 const PONG = 'PONG'
 
-const ping = () => ({ type: PING })
+type State = {
+  isPinging: boolean
+}
 
-const pingEpic = action$ =>
+const initialState: State = { isPinging: false }
+
+interface PingAction {
+  type: typeof PING
+}
+
+interface PongAction {
+  type: typeof PONG
+  payload: number
+}
+
+type Actions = PingAction | PongAction
+
+const ping = (): PingAction => ({ type: PING })
+
+const pingEpic: Epic = action$ =>
   action$.ofType(PING).pipe(
     delay(1000),
     mapTo({ type: PONG })
   )
 
-const pingReducer = (state = { isPinging: false }, action) => {
+const pingReducer = (state = initialState, action: Actions) => {
   switch (action.type) {
     case PING:
       return { isPinging: true }
@@ -30,15 +46,17 @@ const pingReducer = (state = { isPinging: false }, action) => {
   }
 }
 
-let App = ({ isPinging, ping }) => (
+type AppProps = State & { ping: typeof ping }
+
+let App: FunctionComponent<AppProps> = ({ isPinging, ping }) => (
   <div>
     <h1>is pinging: {isPinging.toString()}</h1>
     <button onClick={ping}>Start PING</button>
   </div>
 )
 
-App = connect(
-  ({ isPinging }) => ({ isPinging }),
+const ConnectedApp = connect(
+  ({ isPinging }: State) => ({ isPinging }),
   { ping }
 )(App)
 
@@ -54,7 +72,7 @@ store.subscribe(() => {
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedApp />
   </Provider>,
   document.getElementById('root')
 )
