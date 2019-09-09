3d80097e09dafb5297c1634372bf92d83e977b54
smith-chris <kowalski.k@outlook.com>
2019-09-09 10:05:19 +0200

    Converted pingpong to use redux-solve.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,58 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { createStore } from 'redux'
-// import Counter from './components/Counter'
-// import { reducer, actions } from './reducers'
-import './components/ro'
-// const store = createStore(reducer)
-// const rootEl = document.getElementById('root')
+import React, { FunctionComponent } from 'react'
+import ReactDOM from 'react-dom'
+import { connect, Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { Epic, createEpicMiddleware } from 'redux-observable'
+import { mapTo, delay } from 'rxjs/operators'
+import { makeReducer, makeActionCreators } from 'redux-solve'
 
-// const render = () =>
-//   ReactDOM.render(
-//     <Counter
-//       value={store.getState()}
-//       onIncrement={() => store.dispatch(actions.increment())}
-//       onDecrement={() => store.dispatch(actions.decrement())}
-//     />,
-//     rootEl
-//   )
+type State = {
+  isPinging: boolean
+}
 
-// render()
-// store.subscribe(render)
+const initialState: State = { isPinging: false }
+
+const resolvers = {
+  ping: (state: State) => (): State => ({ ...state, isPinging: true }),
+  pong: (state: State) => (): State => ({ ...state, isPinging: false }),
+}
+
+const actions = makeActionCreators(resolvers, initialState)
+const reducer = makeReducer(resolvers, initialState)
+
+const pingEpic: Epic = action$ =>
+  action$.ofType(actions.ping().type).pipe(
+    delay(1000),
+    mapTo(actions.pong())
+  )
+
+const epicMiddleware = createEpicMiddleware()
+
+const store = createStore(reducer, applyMiddleware(epicMiddleware))
+
+epicMiddleware.run(pingEpic)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+type AppProps = State & typeof actions
+
+let App: FunctionComponent<AppProps> = ({ isPinging, ping }) => (
+  <div>
+    <h1>is pinging: {isPinging.toString()}</h1>
+    <button onClick={ping}>Start PING</button>
+  </div>
+)
+
+const ConnectedApp = connect(
+  ({ isPinging }: State) => ({ isPinging }),
+  actions
+)(App)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
+  document.getElementById('root')
+)
